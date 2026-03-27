13f7f6b6be2362d71f22a2fee78db8599f8423f0
Loic <leotetloic08@gmail.com>
2026-03-27 22:57:59 +0100

    Mise à jour de la page Session.php

---
+++ Core/Session.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Core;
+
+class Session
+
+{
+    public static function set(string $key, mixed $value): void
+
+    {
+        $_SESSION[$key] =$value;
+    }
+
+    public static function get(string $key): mixed
+
+    {
+        return $_SESSION[$key]?? null;
+    }
+
+    public static function destroy(): void
+
+    {
+        session_destroy();
+    }
+
+
+
+
+
+}
